b426652e1acc4b1c69bc371b49ff0165d0848715
yuanzeyv <100289315+yuanzeyv@users.noreply.github.com>
2022-09-13 02:09:25 +0800

    *更新了距离管理单元

--- include/Manager/AxisDistanceManager.h
+++ include/Manager/AxisDistanceManager.h
@@ -4,96 +4,148 @@
 #include <list>     
 #include <unordered_map>
 #include <unordered_set>
-#include <chrono>
+#include <chrono> 
+#include <set>
+#include <algorithm>
 #include <typeinfo>
 #include "Define.h" 
-#include "TemplateBoard.h"  
-//bit 50-64 剩余类型
-//bit 48-49 区域ID
-//bit 00-47  角色ID
-#define GEN_AABB_POINT_TYPE(actorID,type) ((actorID) | ((ActorID)type) << 48) //生成最大最小点类型
-#define GET_ACTOR_ID(actorID) (actorID & (~0xFFFF000000000000))//获取到当前的角色ID
-enum PointPosType {//要不要应该都可以
-	POS_BODY_LIMIT_LEFT_MAX = 0,//左上
-	POS_BODY_LIMIT_LEFT_MIN = 1,//左下 
-	POS_BODY_LIMIT_RIGHT_MAX = 2,//右上 
-	POS_BODY_LIMIT_RIGHT_MIN = 3,//右下 
-	POS_BODY_LIMIT_NUM_MAX = 4,//最大个数
-}; 
+#include "TemplateBoard.h"   
 enum PointType {
-	BODY_TYPE = 0,
-	VIEW_TYPE = 1,
+	BODY_TYPE = 0,//刚体类型
+	VIEW_TYPE = 1,//视图类型
 	MAX_POINT_TYPE = 2,
 };
-struct CompareFloat {
-	bool operator()(const b2Vec2& k1, const b2Vec2& k2) const {
-		if (k1.x < k2.x)
-			return true;
-		if (k1.x > k2.x)
-			return false;
-		return k1.y < k2.y;
-	}
-}; 
-class SmartActorIDMan {
-public:
-	inline void Insert(ActorID actorID, PointPosType type)
-	{ 
-		m_ActorSet.insert(GEN_AABB_POINT_TYPE(actorID, type));
-	}
-	inline void Erease(ActorID actorID, PointPosType type)
-	{ 
-		m_ActorSet.erase(GEN_AABB_POINT_TYPE(actorID, type));
-	}
-	inline const std::unordered_set<ActorID>& GetTable() {
-		return m_ActorSet;
-		
-	}
-	inline void Init() {
-		m_ActorSet.clear();
-	}
-	inline bool IsInValid() { 
-		return !m_ActorSet.size();
-	}
-private: 
-	std::unordered_set<ActorID> m_ActorSet;//记录一组无序的角色ID
+enum PointAxisType {
+	LEFT_BUTTOM = 0,
+	RIGHT_TOP   = 1,
+	MAX_AXIS_TYPE,
 };
-typedef std::map<b2Vec2, SmartActorIDMan*, CompareFloat > ViewRangeRecordMap;//视图轴
+struct b2VecCompare {
+	bool operator()(const b2Vec2*& k1, const b2Vec2*& k2) const {
+		if (k1->x != k2->x)
+			return k1->x < k2->x;
+		return k1->y < k2->y;
+	}
+};  
+class InPosActors{
+public:
+	static inline bool InPosActorsCompare(InPosActors& id1, InPosActors& id2) {
+		if (id1.m_Position.x != id2.m_Position.x)
+			return id1.m_Position.x < id2.m_Position.x; 
+		return id1.m_Position.y < id2.m_Position.y;
+	}
+public:
+	InPosActors() :m_Position() {
+	}
+	b2Vec2 m_Position;
+	std::set<ActorID> m_ActorsSet;
+};
 
-//当前的视图范围
+class AxisMap{
+private: 
+	std::map<b2Vec2*,std::unordered_set<ActorID>, b2VecCompare> m_AxisActors[PointAxisType::MAX_AXIS_TYPE];//记录一组以点位保存的无序的视图对象
+
+	std::vector<InPosActors*> m_RecordArray[PointAxisType::MAX_AXIS_TYPE];//为了防止重复遍历,浪费性能. 在每次准备寻找时,需要将所有map的数据,拷贝到数组中去,以加快性能
+	AutomaticGenerator<b2Vec2> m_b2VecIdleGenerate;//自动的生成视图对象管理的对象 
+public:
+	inline bool AddtionViewRange(ActorID actorID, b2AABB& aabb)
+	{
+		b2Vec2* point[PointAxisType::MAX_AXIS_TYPE];
+		point[PointAxisType::LEFT_BUTTOM] = &aabb.lowerBound;
+		point[PointAxisType::RIGHT_TOP] = &aabb.upperBound; 
+		for (int i = 0; i < PointAxisType::MAX_AXIS_TYPE; i++)
+		{
+			if (m_AxisActors[i].count(point[i]) == 0) {
+				auto b2Vec2Key = m_b2VecIdleGenerate.RequireObj();
+				m_AxisActors[i][b2Vec2Key];
+			}
+			auto actorSet = m_AxisActors[i][point[i]];
+			if (actorSet.count(actorID) == 0)
+				actorSet.insert(actorID);
+		}
+	}
+	inline void DeleteViewRange(ActorID actorID, b2AABB& aabb)
+	{
+		b2Vec2* point[PointAxisType::MAX_AXIS_TYPE];
+		point[PointAxisType::LEFT_BUTTOM] = &aabb.lowerBound;
+		point[PointAxisType::RIGHT_TOP] = &aabb.upperBound;
+		for (int i = 0; i < PointAxisType::MAX_AXIS_TYPE; i++)
+		{
+			if (m_AxisActors[i].count(point[i]) == 0)
+				continue;
+			auto actorSet = m_AxisActors[i][point[i]];
+			if (actorSet.count(actorID))
+				m_AxisActors[i][point[i]].erase(actorID);
+			if (actorSet.size() == 0) {
+				m_b2VecIdleGenerate.BackObj(m_AxisActors[i].find(point[i])->first);
+				m_AxisActors[i].erase(point[i]);
+			}
+		}
+	} 
+	//准备计算了,初始化当前的数据信息
+	inline void ReadyInitActorData()
+	{
+		//初始化数据,由于使用set所以默认是有序的
+		for (int i = 0; i < PointAxisType::MAX_AXIS_TYPE; i++) {
+			auto& recordArray = m_RecordArray[i];
+			auto& actorSet = m_AxisActors[i];
+			recordArray.resize(actorSet.size());//重新设置数组的大小
+			InPosActors** actorData = recordArray.data();//重新设置数组的大小
+			for (auto item = actorSet.begin(); item != actorSet.end(); item++)
+				actorData[i] = *item;
+		}
+	}
+	// 获取到范围内的所有合适的角色
+	inline int GetRangeActors(std::vector<ActorID>& outData, b2AABB& range)
+	{ 
+		std::vector<InPosActors*> compareArr;//当前的数据信息
+		//首先通过2分查找法,快速找到符合要求的那个点位
+		//排除最小点大于 range 的最大点的
+		auto lowPointItor = std::lower_bound(m_RecordArray[PointAxisType::LEFT_BUTTOM].begin(), m_RecordArray[PointAxisType::LEFT_BUTTOM].begin(), range.upperBound); //++
+		auto upPointItor = std::upper_bound(m_RecordArray[PointAxisType::RIGHT_TOP].begin(), m_RecordArray[PointAxisType::RIGHT_TOP].begin(), range.lowerBound);//-- 如果有相同的,直接开始迭代,否则减减后开始迭代
+		std::copy(lowPointItor, m_RecordArray[PointAxisType::LEFT_BUTTOM].end(), compareArr.begin());//从头开始赋值
+		auto midItor = compareArr.end();//获取到前的中间值
+		std::copy(m_RecordArray[PointAxisType::RIGHT_TOP].begin(), upPointItor, compareArr.begin());//从头开始赋值
+		std::inplace_merge(compareArr.begin(), midItor, compareArr.end()); 
+		auto itorEnd = compareArr.end();
+		for (auto item = compareArr.begin(); item != itorEnd; item++) {
+			auto bestItor = std::lower_bound(outData.begin(), outData.end(),*(*item)->m_ActorsSet.begin());//找到元数组最合适的插入位置
+			std::copy((*item)->m_ActorsSet.begin(), (*item)->m_ActorsSet.end(), outData.end());//合并
+			std::inplace_merge(bestItor, outData.begin() + (*item)->m_ActorsSet.size(), outData.end()); 
+		}
+	}                                                                                                                                                                                    
+};
+
 class ViewRange{
 public:
 	ViewRange(BodyData* bodyData,b2Vec2 range = b2Vec2(0.5, 0.5));//视图的初始化
-	BodyData* GetActor(); //获取到当前的角色 
+	BodyData* GetActor();//当前的视图对象
 	const b2Vec2& GetViewRange(); //获取到当前的观察范围
 	const b2Vec2& GetBodyPos();//获取到当前玩家的点位 
-	const std::unordered_map<ActorID, ViewRange*>& GetVisibleMap();//获取到所有可见角色
+	const std::vector<ActorID>& GetObserverArray();//获取到所有可见角色
 	const std::unordered_map<ActorID, ViewRange*>& GetBeObseverView(ViewRange* actor);//某一个角色进入到了被观察的视图
-	const b2AABB& GetBodyAABB() const;//获取到当前角色的刚体AABB   
+	b2AABB& GetBodyAABB() const;//获取到当前角色的刚体AABB   
 	const b2AABB& GetViewAABB() const;//获取到当前角色的刚体AABB 
 	const b2AABB& GetAABB(PointType type) const;//获取到当前角色的刚体AABB 
 
-	bool EnterView(ViewRange* actor);//某一个角色进入视野 
-	void LeaveView(ActorID actorID); //某一个角色离开了视图 
-	bool EnterBeObseverView(ViewRange* actor);//某一个角色进入到了被观察的视图
+	void RefreshObserver(std::vector<ActorID>& actorArr);//每一帧只要进入的,就会直接刷新 
 
-	bool InObserverContain(ViewRange* actor); //计算制定AABB是包含自己
-	void ClearObserverTable();//清理当前观察到的所有角色
+	bool InObserverContain(ViewRange* actor); //计算制定AABB是包含自己 
 
-	int IsRefreshActorView();//计算视差,大于多少才会重计算 
-	void MoveSelf(); //通知每个观察到自己的人,当前自己移动了  
+	int IsRefreshActorView();//计算视差,大于多少才会重计算  
 	void RecalcBodyPosition();//重新获取到角色上一次未刷新的位置
 
 	void RecalcBodyAABB();//重新计算视口 
 	void RecalcViewAABB();//重新计算视口
 	void RecalcAABB(PointType type);//重新计算视口
+
 	void RecalcRefreshCondtion();//重计算刷新条件    
 	~ViewRange() {}
 private:
 	BodyData* m_Actor;//当前的角色 
 	b2Vec2 m_BodyPos;//上一次的角色位置
 
-	std::unordered_map<ActorID, ViewRange*> m_ObserverMap;//可以看到的角色列表  
-	std::unordered_map<ActorID, ViewRange*> m_BeObserverMap;//用于记录每个角色被谁观察到 
+	std::vector<ActorID> m_ObserverArr;//可以看到的角色列表   
 	b2Vec2 m_ObserverRange;//当前角色可以观察到的范围  
 
 	b2AABB m_AABB[PointType::MAX_POINT_TYPE];//记录角色上一次添加时的AABB      
@@ -108,37 +160,33 @@ public:
 	void UnregisterBody(ActorID id);//反注册
 	void UnregisterBody(BodyData* actor);//反注册
 	//计算区域 
-	void AddDelayCalcTable(ActorID id);//添加一个刚体计算对象 
-	void AdditionAABB(PointType type,ViewRange* actoViewRange);//添加一个视图AABB
-	void RemoveAABB(PointType type,ViewRange* actoViewRange);
+	void AddDelayCalcTable(ActorID id);//添加一个刚体计算对象  
 	//获取到单元
 	ViewRange* GetViewRange(ActorID actorID);
 	//重计算
 	void Update();//重新计算角色距离
 	void ActorsMove();
-	void CalcViewObj();//重计算
-
+	void CalcViewObj();//重计算 
 	static AxisDistanceManager& Instance();
 private:
 	AxisDistanceManager() {}
 	//寻找某一个轴的某一范围内的所有单元信息
 	void InquiryAxisPoints(std::unordered_set<ActorID>& outData,const b2AABB& viewAABB);
 private:
-	//内存分配对象 
-	AutomaticGenerator<SmartActorIDMan> m_ViewCellIdleGenerate;//自动的生成视图对象管理的对象 
 	//当前所有的距离对象
-	ViewRangeRecordMap m_AxisBodyMap[PointType::MAX_POINT_TYPE];//其中有X坐标列和Y坐标列 
+	AxisMap m_AxisBodyMap;//其中有X坐标列和Y坐标列 
 	std::unordered_map<ActorID, ViewRange*> m_ViewObjMap;//角色对应的范围信息 
 
 	std::unordered_map<ActorID, ViewRange*> m_DelayCalcMoveList;//角色对应的范围信息
-	std::unordered_map<ActorID, ViewRange*> m_CalcMoveList;//应该被计算的列表
+	std::vector<ViewRange*> m_CalcMoveList;//应该被计算的列表
 }; 
 
-inline void ViewRange::ClearObserverTable() //清理当前观察到的所有角色
-{  
-	m_ObserverMap.clear();
-}
-
+inline void ViewRange::RefreshObserver(std::vector<ActorID>& actorArr)
+{ 
+	m_ObserverArr.resize(actorArr.size());
+	memcpy(m_ObserverArr.data(), actorArr.data(),sizeof(actorArr.size() * sizeof(ActorID)));
+	AxisDistanceManager& manager = AxisDistanceManager::Instance();  
+} 
 inline const b2Vec2& ViewRange::GetViewRange() //获取到当前的视图范围
 {
 	return m_ObserverRange;
@@ -146,12 +194,8 @@ inline const b2Vec2& ViewRange::GetViewRange() //获取到当前的视图范围
 inline const b2Vec2& ViewRange::GetBodyPos()//获取到当前玩家的点位
 {
 	return m_BodyPos;
-}
-inline void ViewRange::LeaveView(ActorID actorID) //某一个角色离开了视图
-{
-	m_ObserverMap.erase(actorID);
-}
-inline const b2AABB& ViewRange::GetBodyAABB() const//获取到当前角色的刚体AABB 
+} 
+inline b2AABB& ViewRange::GetBodyAABB() const//获取到当前角色的刚体AABB 
 {
 	return m_AABB[PointType::BODY_TYPE];
 }
@@ -192,12 +236,9 @@ inline BodyData* ViewRange::GetActor() {//获取到当前的角色
 	return m_Actor;
 }
 
-inline bool ViewRange::InObserverContain(ViewRange* actor) {//移动的角色询问当前是否还在监听范围内 
-	if (!b2TestOverlap(m_AABB[PointType::VIEW_TYPE], actor->GetBodyAABB())) {//如果当前看得观察到了这个刚体,但现在看不见了
-		m_ObserverMap.erase(actor->m_Actor->ID());//执行删除
-		return false;
-	}
-	return true;//删除自己
+inline bool ViewRange::InObserverContain(ViewRange* actor) {//移动的角色询问当前是否还在监听范围内  
+	auto startItor = std::lower_bound(m_ObserverArr.begin(), m_ObserverArr.end(),actor->GetActor()->ID());
+	return startItor != m_ObserverArr.end() && *startItor == actor->GetActor()->ID(); 
 }
 inline const b2AABB& ViewRange::GetAABB(PointType type) const//获取到当前角色的刚体AABB 
 {
@@ -212,29 +253,11 @@ inline int ViewRange::IsRefreshActorView()//计算视差,大于多少才会重计算
 		return 1;
 	return 0;
 }
-inline bool ViewRange::EnterView(ViewRange* actor)//某一个角色进入视野
+inline const std::vector<ActorID>& ViewRange::GetObserverArray()//获取到所有可见角色
 {
-	m_ObserverMap[actor->GetActor()->ID()] = actor;//我观察到了别人
-	actor->EnterBeObseverView(this);//别人被我观察到了
-	return true;
+	return this->m_ObserverArr;
 }
-inline const std::unordered_map<ActorID, ViewRange*>& ViewRange::GetVisibleMap()//获取到所有可见角色
-{
-	return this->m_ObserverMap;
-}
-
-inline bool ViewRange::EnterBeObseverView(ViewRange* actor)//某一个角色进入到了被观察的视图
-{
-	if (m_BeObserverMap.count(actor->GetActor()->ID()))//存在就不创建
-		return true;
-	m_BeObserverMap[actor->GetActor()->ID()] = actor;
-	return true;
-}
-
-inline const std::unordered_map<ActorID, ViewRange*>& ViewRange::GetBeObseverView(ViewRange* actor)//某一个角色进入到了被观察的视图
-{
-	return m_BeObserverMap;
-}
+  
 
 inline void AxisDistanceManager::AddDelayCalcTable(ActorID id) { 
 	if (m_DelayCalcMoveList.count(id))
@@ -268,6 +291,8 @@ inline void AxisDistanceManager::UnregisterBody(BodyData* actor)
 }
 //重新计算角色距离
 inline void AxisDistanceManager::Update() {
+	//针对所有移动过的角色重新计算他们的可视范围内的所有 
+
 	int64_t moveCost;
 	int64_t calcCost;
 	auto t1 = std::chrono::high_resolution_clock::now();
--- src/Manager/AxisDistanceManager.cpp
+++ src/Manager/AxisDistanceManager.cpp
@@ -6,123 +6,41 @@ ViewRange::ViewRange(BodyData* bodyData, b2Vec2 range) :
 	m_Actor(bodyData), 
 	m_OffsetCondtion(-9999,-9999){
 } 
-
-//自己移动的话
-void ViewRange::MoveSelf() {//通知每个观察到自己的人,当前自己移动了
-	for (auto item = m_BeObserverMap.begin(); item != m_BeObserverMap.end(); ) {
-		if (!item->second->InObserverContain(this))//如果自己当前还在对方的视野下
-			m_BeObserverMap.erase(item++);
-		else
-			item++;
-	}
-} 
+ 
  
 void AxisDistanceManager::ActorsMove()
 {
 	ViewRange* actorView = NULL;//首先移动所有的角色
 	bool isDelete = false;//当前是否仅仅计算  
-	auto iterEnd = m_DelayCalcMoveList.end();
-	for (auto item = m_DelayCalcMoveList.begin(); item != iterEnd;item++) { 
-		ActorID actorID = item->first; 
-		actorView = item->second;
-		if (actorView->IsRefreshActorView() == 0) {//移动距离过浅
+	auto iterEnd = m_DelayCalcMoveList.end();//防止重复获取到end
+	for (auto item = m_DelayCalcMoveList.begin(); item != iterEnd;item++) { //循环遍历到所有的移动的角色
+		ActorID actorID = item->first; //获取到角色ID
+		actorView = item->second;//获取到角色的视图数据
+		if(actorView->IsRefreshActorView() == 0)//移动距离过浅
 			continue;
-		} 
 		actorView->RecalcBodyPosition();//重计算角色的位置  
-		RemoveAABB(PointType::BODY_TYPE, actorView);
-		RemoveAABB(PointType::VIEW_TYPE, actorView); 
+		//开始删除角色上一帧的信息
+		m_AxisBodyMap.DeleteViewRange(actorID, actorView->GetBodyAABB()); 
 		actorView->RecalcBodyAABB();
-		actorView->RecalcViewAABB();
-		AdditionAABB(PointType::BODY_TYPE, actorView);
-		AdditionAABB(PointType::VIEW_TYPE, actorView);
-		m_CalcMoveList[actorID] = item->second;
+		m_AxisBodyMap.AddtionViewRange(actorID, actorView->GetBodyAABB());
+		m_CalcMoveList.push_back(item->second);
 	}  
 	m_DelayCalcMoveList.clear(); 
 }
-
-
-void AxisDistanceManager::AdditionAABB(PointType type, ViewRange* actoViewRange)
-{
-	const b2AABB& viewAABB = actoViewRange->GetAABB(type);//获取到对应类型的AABB
-	b2Vec2* posPoint[PointPosType::POS_BODY_LIMIT_NUM_MAX] = {
-		&b2Vec2(viewAABB.lowerBound.x, viewAABB.upperBound.y),//POS_BODY_LIMIT_LEFT_MAX
-		&b2Vec2(viewAABB.lowerBound.x, viewAABB.lowerBound.y),//POS_BODY_LIMIT_LEFT_MIN
-		&b2Vec2(viewAABB.upperBound.x, viewAABB.upperBound.y),//POS_BODY_LIMIT_RIGHT_MAX
-		&b2Vec2(viewAABB.upperBound.x, viewAABB.lowerBound.y) }; //POS_BODY_LIMIT_RIGHT_MIN  
-	ActorID actorID = actoViewRange->GetActor()->ID();
-	ViewRangeRecordMap& axisCell = m_AxisBodyMap[type];
-	for (int i = 0; i < PointPosType::POS_BODY_LIMIT_NUM_MAX; i++) {
-		b2Vec2& pos = *posPoint[i];
-		if (!axisCell.count(pos)) {
-			axisCell[pos] = m_ViewCellIdleGenerate.RequireObj();
-		}
-		axisCell[pos]->Insert(actorID, (PointPosType)i);
-	} 
-}
-void AxisDistanceManager::RemoveAABB(PointType type, ViewRange* actoViewRange)
-{
-	const b2AABB& viewAABB = actoViewRange->GetAABB(type);//获取到对应类型的AABB
-	b2Vec2* posPoint[PointPosType::POS_BODY_LIMIT_NUM_MAX] = {
-		&b2Vec2(viewAABB.lowerBound.x, viewAABB.upperBound.y),//POS_BODY_LIMIT_LEFT_MAX
-		&b2Vec2(viewAABB.lowerBound.x, viewAABB.lowerBound.y),//POS_BODY_LIMIT_LEFT_MIN
-		&b2Vec2(viewAABB.upperBound.x, viewAABB.upperBound.y),//POS_BODY_LIMIT_RIGHT_MAX
-		&b2Vec2(viewAABB.upperBound.x, viewAABB.lowerBound.y) };//POS_BODY_LIMIT_RIGHT_MIN 
-	ViewRangeRecordMap& axisCell = m_AxisBodyMap[type];
-	ActorID actorID = actoViewRange->GetActor()->ID();
-	for (int i = 0; i < PointPosType::POS_BODY_LIMIT_NUM_MAX; i++) {
-		b2Vec2& pos = *posPoint[i];
-		if (!axisCell.count(pos))//首先判断对应类型是否有当前信息
-			continue;
-		axisCell[pos]->Erease(actorID, (PointPosType)i);//删除这个单元
-		if (axisCell[pos]->IsInValid()) {
-			m_ViewCellIdleGenerate.BackObj(axisCell[pos]);
-			axisCell.erase(pos);
-		}
-	}
-}
-extern ActorID playerID;
+  
 //寻找某一个轴的某一范围内的所有单元信息  
 void AxisDistanceManager::InquiryAxisPoints(unordered_set<ActorID>& outData,const b2AABB& viewAABB)//偏移单元代表当前要查询的息(必须确保当前被查询点存在于表内)
-{ 
-	b2Vec2 tempPoint = viewAABB.lowerBound;//获取到当前的视图最低点
-	ViewRangeRecordMap::iterator iterEnd = m_AxisBodyMap[PointType::BODY_TYPE].end();//获取到小于等于当前值的东西  
-	ViewRangeRecordMap::iterator iter = m_AxisBodyMap[PointType::BODY_TYPE].lower_bound(tempPoint);
-	while (iter != iterEnd && iter->first.x <= viewAABB.upperBound.x)//末尾 或者 越界
-	{	
-		while (iter != iterEnd && (iter->first.y > viewAABB.upperBound.y || iter->first.y < viewAABB.lowerBound.y))//在这个范围内的话,就不停的寻找下一个
-		{
-			if (iter->first.y > viewAABB.upperBound.y) {
-				tempPoint.Set(iter->first.x, 99999);
-				iter = m_AxisBodyMap[PointType::BODY_TYPE].upper_bound(tempPoint);//查找x轴
-			} 
-			if (iter != iterEnd && iter->first.y < viewAABB.lowerBound.y) {
-				tempPoint.Set(iter->first.x, viewAABB.lowerBound.y); 
-				iter = m_AxisBodyMap[PointType::BODY_TYPE].lower_bound(tempPoint);//查找x轴
-			}
-		}
-		if (iter == iterEnd) break;
-		for (auto item = iter->second->GetTable().begin(); item != iter->second->GetTable().end(); item++) {
-			ActorID id = GET_ACTOR_ID(*item);
-			if (outData.count(id))
-				continue;
-			outData.insert(id); 
-		}
-		iter++;
-	} 
+{  
 }
 //重计算
 void AxisDistanceManager::CalcViewObj()
-{
-	//每个角色先移动一下自己,刷新当前的被刷新对象
-	for (auto actorItem = m_CalcMoveList.begin(); actorItem != m_CalcMoveList.end(); actorItem++)
-		actorItem->second->MoveSelf();//代表自己移动了
-
+{ 
 	//重新计算自己所见所得
 	ViewRange* actoViewange = NULL;
 	for (auto item = m_CalcMoveList.begin(); item != m_CalcMoveList.end(); item++) {
 		//首先计算当前的操作 
-		ActorID actorID = item->first; 
-		actoViewange = item->second;
+		ActorID actorID = (*item)->GetActor()->ID();
+		actoViewange = *item;
 		actoViewange->ClearObserverTable();//清除,然后重新计算
 		unordered_set<ActorID> XSet;
 		const b2AABB& viewAABB = actoViewange->GetViewAABB();
